0d9652169fe03f4d7b28a214717207a64cc6683c
Deepak kumar <dkumar39223@gmail.com>
2024-12-22 16:43:16 +0530

    Lecture-15 setup

---
+++ Lecture-15(Method chaining)/script.js
@@ -0,0 +1,13 @@
+// Method Chaining = calling one method after another in one
+//                   continuous line of code.
+
+let username = window.prompt("Enter your username: ");
+
+
+//----------No Method Chaining------------
+
+
+
+
+
+//---------Method Chaining------------
