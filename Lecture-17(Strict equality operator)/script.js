53187ddd101fecc4525fac549ace34507cc05cbb
Deepak kumar <dkumar39223@gmail.com>
2024-12-22 19:47:30 +0530

    Lecture-17 strict equality operator

---
+++ Lecture-17(Strict equality operator)/script.js
@@ -0,0 +1,35 @@
+//    = assignment operator
+//    == comparsion operator (compare if values are equal)
+//    === strict equality operator (compare if values & datatype are equal)
+//     !=  inequality operator
+//     !== strict inequality operator
+
+
+const PI = 3.14;
+
+// if (PI == "3.14") {
+//     console.log("That is Pi");  
+// } else {
+//     console.log("That is not Pi");
+// }
+
+
+// if (PI === "3.14") {
+//     console.log("That is Pi");  
+// } else {
+//     console.log("That is not Pi");
+// }
+
+
+// if (PI != "3.14") {
+//     console.log("That is not Pi");  
+// } else {
+//     console.log("That is  Pi");
+// }
+
+
+if (PI !== "3.14") {
+    console.log("That is not Pi");  
+} else {
+    console.log("That is  Pi");
+}
