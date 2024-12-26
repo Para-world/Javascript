b755dcfda65e7b88a816efcb9b9c4afa15dd0d03
Deepak kumar <dkumar39223@gmail.com>
2024-12-26 20:52:29 +0530

    Lecture-18 compelte

--- Lecture-18(While Loops)/script.js
+++ Lecture-18(While Loops)/script.js
@@ -1,2 +1,36 @@
-// While Loops = repeat some code While some conditon is ture
+// While Loops = repeat some code While some condition is true
+
+// Example 1: Print numbers from 1 to 5
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+// Example 2: Print even numbers from 2 to 10
+let j = 2;
+while (j <= 10) {
+    console.log(j);
+    j += 2;
+}
+
+// Example 3: Print numbers from 10 to 1 in reverse order
+let k = 10;
+while (k >= 1) {
+    console.log(k);
+    k--;
+}
+
+// Example 4: Sum of numbers from 1 to 5
+let sum = 0;
+let l = 1;
+while (l <= 5) {
+    sum += l;
+    l++;
+}
+console.log("Sum of numbers from 1 to 5 is: " + sum);
+
+
+
+
 
