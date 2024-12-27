155d3e00a5b1b7747b6a395d1d0b8a7c62287de7
Deepak kumar <dkumar39223@gmail.com>
2024-12-27 21:41:07 +0530

    Lecture-019

---
+++ Lecture-019(for loop)/script.js
@@ -0,0 +1,25 @@
+// for loop =  repeat some code a Limited amount of times
+
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+console.log("happy new year");
+
+
+// for (let i = 0; i <= 20; i++) {
+//     if(i == 13) {
+//         continue
+//     }else{
+//         console.log(i);
+//     }
+  
+// }
+
+for (let i = 0; i <= 20; i++) {
+    if(i == 13) {
+        break
+    }else{
+        console.log(i);
+    }
+  
+}
