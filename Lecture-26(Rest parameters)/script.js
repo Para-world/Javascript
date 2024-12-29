8c253e0863ffc309a894c038a370705a90cb6507
Deepak kumar <dkumar39223@gmail.com>
2024-12-29 20:35:47 +0530

    Lecture-26 setup

---
+++ Lecture-26(Rest parameters)/script.js
@@ -0,0 +1,7 @@
+//rest parameters = (...rest) allow a function work with an vaiable
+//                  number of arguments by bundling then into an array
+
+//                  spread = expands an array into spereate elements
+//                   rest = condenses spereate elements into an array
+
+
