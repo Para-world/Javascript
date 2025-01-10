8131bc1424442d5b83baebc86c58fa0acfe37e4a
Deepak kumar <dkumar39223@gmail.com>
2025-01-10 10:04:24 +0530

    Lecture-37 compelte

--- Lecture-37 What is THIS/script.js
+++ Lecture-37 What is THIS/script.js
@@ -14,5 +14,18 @@ const person1 = {
     },
 };
 
+const person2 = {
+    name: 'John wick',
+    favfood: 'chole bhture',
+    sayhello: function () {
+        console.log(`hi i am ${this.name}`);
+        },
+        eat: function () {
+            console.log(`i am eating ${this.favfood}`);
+            }
+        };
+
 person1.sayhello(); 
-person1.eat();      
+person1.eat();  
+person2.sayhello();
+person2.eat();  
